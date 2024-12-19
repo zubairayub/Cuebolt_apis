3960ca6ca0437b6da2305a52013077cde1cf6350
Zubair Ayub <zubairayub@hotmail.com>
2024-12-19 15:03:48 +0500

    update

--- database/migrations/2024_12_17_104110_add_fields_to_packages_table.php
+++ database/migrations/2024_12_17_104110_add_fields_to_packages_table.php
@@ -16,10 +16,7 @@ return new class extends Migration
             $table->boolean('is_challenge')->default(false); // Adds the challenge boolean
             $table->foreignId('market_type_id')->constrained('trading_markets')->onDelete('cascade'); // Foreign key to trading_markets
         }
-            $table->decimal('achieved_rrr', 5, 2)->nullable()->default(null); // Nullable decimal for achieved RRR
-            $table->decimal('from_amount', 10, 2)->nullable()->default(null); // Nullable decimal for from_amount
-            $table->decimal('to_amount', 10, 2)->nullable()->default(null); // Nullable decimal for to_amount
-            $table->integer('challenge_days')->nullable()->default(null); // Nullable integer for challenge_days
+            
             
         });
     }
@@ -34,10 +31,7 @@ return new class extends Migration
              $table->dropColumn([
                 'is_challenge',
                 'market_type_id',
-                'achieved_rrr',
-                'from_amount',
-                'to_amount',
-                'challenge_days',
+                
             ]);
         });
     }
