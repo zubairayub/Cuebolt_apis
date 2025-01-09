87743abbe11b2126d5c4ab7fb01182e7993ad0e2
Zubair Ayub <zubairayub@hotmail.com>
2025-01-09 18:00:39 +0500

    badge image fix

--- app/Models/Badge.php
+++ app/Models/Badge.php
@@ -19,7 +19,7 @@ class Badge extends Model
     public function getIconUrlAttribute()
     {
         return $this->icon 
-            ? asset('storage/app/public/' . $this->icon)  
+            ? asset('storage/app/public/uploads/images/badges/' . $this->icon)  
             : asset('storage/app/public/uploads/images/badges/default_badge_icon.png');
     }
 }
